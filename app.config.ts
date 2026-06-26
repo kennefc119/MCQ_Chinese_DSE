57ae3e2627e82025bc660d6f52d025b2c8a4123a
Kenneth Cheung <kennefcc1119@protonmail.com>
2026-06-27 02:53:15 +0800

    chaneg config to 1.0.6

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "文淵DSE",
   slug: "dsemcq",
-  version: "1.0.5",
+  version: "1.0.6",
   scheme: "dsemcq",
   orientation: "portrait",
   userInterfaceStyle: "dark",
